b5ee89b0aebfa515711b6ecfa12079e55478c817
dzsef <nemethmarcell357@gmail.com>
2025-09-07 14:26:12 +0200

    backend - models

---
+++ apps/api/app/models/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Boolean, Column, DateTime, Integer, String, func
+from app.db.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(320), unique=True, index=True, nullable=False)
+    hashed_password = Column(String(255), nullable=False)
+    is_active = Column(Boolean, default=True)
+    is_admin = Column(Boolean, default=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
