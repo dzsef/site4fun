230349be1748ec7f64ccfd0ce130115dd38267e8
dzsef <nemethmarcell357@gmail.com>
2025-09-20 09:44:06 +0200

    add i18n

--- apps/web/src/i18n.ts
+++ apps/web/src/i18n.ts
@@ -1,14 +1,22 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import hu from '../public/locales/hu/common.json';
-import en from '../public/locales/en/common.json';
+import hu from './translations/hu.json';
+import en from './translations/en.json';
 
-i18n.use(initReactI18next).init({
-  resources: { hu: { translation: hu }, en: { translation: en } },
-  lng: 'hu',
-  fallbackLng: 'en',
-  interpolation: { escapeValue: false },
-});
+// Initialize i18next with translations and default language
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      hu: { translation: hu },
+      en: { translation: en },
+    },
+    lng: 'hu',
+    fallbackLng: 'hu',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 export default i18n;
