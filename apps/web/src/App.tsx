55af3c3e4e6ff0e11d0f2454bcbb5dae6e524d6e
dzsef <nemethmarcell357@gmail.com>
2025-09-20 09:44:15 +0200

    add new routes

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,28 +1,37 @@
-import { Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Services from './pages/Services';
 import References from './pages/References';
 import RequestQuote from './pages/RequestQuote';
-import Privacy from './pages/Privacy';
-import Header from './components/Header';
+import NotFound from './pages/NotFound';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import CookieBanner from './components/CookieBanner';
 
-export default function App() {
+const App: React.FC = () => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/references" element={<References />} />
-          <Route path="/request-quote" element={<RequestQuote />} />
-          <Route path="/privacy" element={<Privacy />} />
-        </Routes>
-      </main>
-      <Footer />
-      <CookieBanner />
-    </div>
+    <BrowserRouter>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/references" element={<References />} />
+            <Route path="/request-quote" element={<RequestQuote />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
-}
+};
+
+export default App;
