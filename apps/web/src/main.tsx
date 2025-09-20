1bb708bd6728e9db4953a747ed504e53a4abd0b4
dzsef <nemethmarcell357@gmail.com>
2025-09-20 09:44:57 +0200

    src & nginx files

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import './i18n';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 import App from './App';
+import './index.css';
+
+// Create a QueryClient instance for React Query
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <I18nextProvider i18n={i18n}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </I18nextProvider>
   </React.StrictMode>
 );
